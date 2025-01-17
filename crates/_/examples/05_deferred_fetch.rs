f327e7ae02357e72e439bf626dc956b5bb5cea39
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2025-01-17 10:50:01 +0100

    fixed fetching example

--- crates/_/examples/05_deferred_fetch.rs
+++ crates/_/examples/05_deferred_fetch.rs
@@ -8,9 +8,8 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut database = AssetDatabase::default()
         .with_protocol(BytesAssetProtocol)
-        // Deferred asset fetch runs fetching jobs in threads.
+        // Deferred asset fetch runs fetching jobs in threads for any fetch engine.
         .with_fetch(DeferredAssetFetch::new(
-            // File asset fetch implements deferred job mechanism.
             FileAssetFetch::default().with_root("resources"),
         ));
 
